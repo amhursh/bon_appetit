d53bd24b67ec3d92b36c4c466676c5cc15f71783
amhursh <amhursh@gmail.com>
2017-08-01 09:46:37 -0600

    Build number converter

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -19,4 +19,20 @@ class Pantry
     end
   end
 
+  def determine_amount(num)
+    return num = (num / 100.0) if num > 100
+    return num = (num * 1000.0) if num < 1
+    return num
+  end
+
+  def determine_units(num)
+    return "Centi-Units" if num > 100
+    return "Milli-Units" if num < 1
+    return "Universal-Units"
+  end
+
+  # def convert_units(recipe)
+  #   converted = recipe.map do
+  # end
+
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,4 +1,5 @@
 require './lib/pantry'
+require './lib/recipe'
 require 'minitest/autorun'
 require 'minitest/pride'
 
@@ -38,6 +39,32 @@ class PantryTest < Minitest::Test
     pantry.restock("Cheese", 5)
 
     assert_equal 15, pantry.stock_check("Cheese")
+  end
+
+  def test_unit_conversion
+    pantry = Pantry.new
+    amount = 120
+    amount_two = 0.234
+    amount_three = 49
+
+    assert_equal 1.2, pantry.determine_amount(amount)
+    assert_equal 234, pantry.determine_amount(amount_two)
+    assert_equal 49, pantry.determine_amount(amount_three)
+  end
+
+  def test_pantry_can_convert_units
+    skip
+    r = Recipe.new("Spicy Cheese Pizza")
+    r.add_ingredient("Cayenne Pepper", 0.025)
+    r.add_ingredient("Cheese", 75)
+    r.add_ingredient("Flour", 500)
+    pantry = Pantry.new
+    converted = pantry.convert_units(r)
+
+    assert_instance_of Hash, converted
+    assert_equal ({"Cayenne Pepper" => {quantity: 25, units: "Milli-Units"},
+                  "Cheese" => {quantity: 75, units: "Universal Units"},
+                  "Flour" => {quantity: 5, units: "Centi-Units"}}), converted
   end
 
 end
